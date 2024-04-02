480fc2ed5f7cfd69fba1b8212d4244a2e3c60dcc
CodeDDAGGI <154308455+CodeDDAGGI@users.noreply.github.com>
2024-04-02 21:50:18 +0900

    Update generic-test.ts

--- generic-test/generic-test.ts
+++ generic-test/generic-test.ts
@@ -15,7 +15,6 @@ class Student {
 
   constructor (
   public name : string,
-  public stnum : number,
   public age : number,
   public grade : string,
   public record : number
@@ -26,30 +25,76 @@ class StudentMain<T extends Student>{
   private students : T[] = []; // Student 객체들을 저장할 배열
 
   //? 학생 추가
+  // push() : 배열 끝에 요소 추가
   addStudent(student:T){
     this.students.push(student);
   }
 
   //? 학생 이름을 기준으로 검색
-  findStudent(name:string, stnum: number):T |undefined{
+  // find() : 주어진 조건을 만족하는 배열의 첫 번째 요소 반환
+  findStudent(name:string):T |undefined{
     return this.students.find(student=>student.name === name);
   }
 
-  //? 미응시자 학생 제거-성적(record)기준
-  removeStudent(record:number):void{
-    let index = this.students.findIndex(student => student.record === record);
+  //? 학생 제거-성적(record)기준
+  // findIndex() : 주어진 조건을 만족하는 배열의 첫 번째 요소의 인덱스 반환
+  // splice() : 배열의 요소를 추가하거나 제거, 제거된 요소를 반환 
+  removeStudent(name :string):void{
+    let index = this.students.findIndex(student => student.name === name);
     if(index !== -1){
       this.students.splice(index, 1);
     }
   }
 
   //? 학생 성적순 나열
+  // sort() 메서드 : 배열의 요소를 정렬
+  // slice() 메서드 : 배열의 일부분을 추출하여 새로운 배열을 반환
   filterRecordStudent(): T[]{
-    return this.students.sort(student => student.record)
+    return this.students.slice().sort((a,b) => b.record - a.record)
   }
 
+
+
   //? 학생 성적 평균 계산
+  // 배열의 각 요소에 대해 콜백함수 실행, 하나의 결과값을 반환
   StudentAverage():number{
-    if
+    if (this.students.length === 0) return 0;
+    
+    const totalRecord = this.students.reduce((acc, cur) => acc + cur.record,0);
+    let average = 0;
+
+    return average / this.students.length;
   }
+
+  filterHeightRecord():T[]{
+    const sortStudents = this.students.sort((a,b) => b.record-a.record);
+    const topStudents = sortStudents.slice(0,3);
+    return topStudents;
+  }
+
+  
 }
+
+
+let StudentManage = new StudentMain<Student>();
+
+StudentManage.addStudent(new Student ('홍길동',16,'A',90));
+StudentManage.addStudent(new Student ('김길동',17,'B',85));
+StudentManage.addStudent(new Student ('고길동',16,'D',55));
+StudentManage.addStudent(new Student ('이길동',17,'C',72));
+
+console.log(StudentManage.findStudent('홍길동'));
+console.log(StudentManage.findStudent('고길동'));
+console.log(StudentManage.findStudent('일지매'));
+
+StudentManage.removeStudent('홍길동');
+console.log(StudentManage.findStudent('홍길동'));
+
+StudentManage.StudentAverage()
+StudentManage.filterHeightRecord();
+// 성적 순으로 나열
+// forEach() : 배열의 각 요소에 대해 주어진 함수를 실행 (for문이랑 느낌 비슷)
+const sortStudents = StudentManage.filterRecordStudent();
+sortStudents.forEach(student => {
+  console.log(`이름 : ${student.name} , 성적 : ${student.record}`);
+})
