3dd31e3213dc78902b0b1a757c489a942a7eba63
CodeDDAGGI <154308455+CodeDDAGGI@users.noreply.github.com>
2024-04-12 19:44:20 +0900

    Add files via upload

---
+++ practice/dataService.ts
@@ -0,0 +1,21 @@
+import {User, Post} from './main';
+
+// import {fetchData} from './apiUtils'
+
+async function getUser(userId: number) {
+  const user = await 
+  fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+
+  return user;
+
+}
+
+async function getPostsByUser(userId :number) {
+  const user = await fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`)
+
+  return user
+}
+
+export{getUser , getPostsByUser};
+console.log(getUser(1));
+console.log(getPostsByUser(1));
