2d06fbb01854af789763fcf0810e6ff6e1174e89
CodeDDAGGI <154308455+CodeDDAGGI@users.noreply.github.com>
2024-04-19 22:30:55 +0900

    Add files via upload

---
+++ 0414/b_Props/Props02.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+/*
+! 두 개의 컴포넌트 구성
+
+UserProfile 컴포넌트
+: 두 가지 props를 전달 완료
+  - username: 문자열
+  - age: 숫자
+  >> 이름과 나이를 전달 받고 '안녕하세요, {username}님, 당신의 나이는 {age}살 입니다.'
+
+  Message 컴포넌트
+  : 하나의 props를 전달 받음
+    - text : 문자열
+    >> 메시지를 단순하게 화면에 표시
+    '오늘도 좋은 하루 되세요'
+    
+! 부모 컴포넌트
+: UserProfile과 Message 컴포넌트를 사용하여 사용자 프로필과 환영 메시지를 표시
+
+*/
+
+type UserProfileType ={
+  username: string;
+  age: number;
+}
+
+
+const UserProfile: React.FC<UserProfileType>=({username, age}) => {
+  return (
+    <div style={{ fontWeight: 'bold', margin: '10px'}}>
+      안녕하세요, {username}님, 당신의 나이는 {age}살 입니다.
+    </div>
+  )
+}
+
+type MessageType = {
+  text: string;
+}
+
+function Message({text}: MessageType) {
+  return(
+    <div style={{color:'green' , margin: '10px'}}>
+      {text}
+    </div>
+  )
+}
+
+Message.defaultProps = {
+  text: '전달할 내용이 없습니다.'
+}
+
+export default function Props02() {
+  return (
+    <div>
+      <UserProfile username='이승아' age={28}/>
+      <Message text='오늘도 좋은 하루 되세요!'/>
+    </div>
+  )
+}
+
---
+++ 0414/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import JSX01 from './a_JSX/JSX01'
+import JSX02 from './a_JSX/JSX02'
+import JSX03 from './a_JSX/JSX03'
+import Props01 from './b_Props/Props01'
+import Props02 from './b_Props/Props02'
+import Rendering01 from './c_Rendering/Rendering01'
+import Rendering02 from './c_Rendering/Rendering02'
+import Practice0414 from './Practice0414'
+
+export default function index() {
+  return (
+    <div>
+      <h1 
+        // CSS의 경우 속성 사용 시
+        // : background-color로 지정
+      style={{ backgroundColor : 'pink' }}
+      >
+        0414 수업자료
+        </h1>
+        <h2>1. JSX</h2>
+        <JSX01 />
+        <JSX02 />
+        <JSX03 />
+
+        <h2>2. Props</h2>
+        <Props01 />
+        <Props02 />
+
+        <h2>3. 렌더링(조건부/리스트)</h2>
+        <Rendering01 />
+        <Rendering02 />
+        <Practice0414 />
+    </div>
+  )
+}
