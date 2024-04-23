1b65b583176e47eddc1d2099d04cf01e700981fc
CodeDDAGGI <154308455+CodeDDAGGI@users.noreply.github.com>
2024-04-23 21:01:53 +0900

    Add files via upload

---
+++ mini project/todo/starbugs/login/starbug.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    const member = document.querySelector(".member");
+
+    member.addEventListener('click',()=>{
+      window.open("","_blank");
+    })
+})
+
+const login = document.querySelector(".login");
+
+login.addEventListener('click' , ()=>{
+  window.open("","_blank");
+})
+
+const giftpresent = document.querySelector(".starbugs-giftcard");
+
+giftpresent.addEventListener('click', ()=>{
+  window.open("","_blank");
+})
