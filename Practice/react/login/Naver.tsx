c600de20bef2c0ffe2cbe62e372eb31346b9c8f8
CodeDDAGGI <154308455+CodeDDAGGI@users.noreply.github.com>
2024-04-27 00:26:08 +0900

    Update Naver.tsx

--- Practice/react/login/Naver.tsx
+++ Practice/react/login/Naver.tsx
@@ -5,39 +5,155 @@ export default function Naver() {
   return (
     <>
     <div style={{
-      
+      width:'100%',
+      display: 'flex',
+      flexDirection:'column',
+      marginTop:'21px',
     }}>
-      <div>
-      <select name="lan" id="lan" 
-      style={{
-        border: '1px solid rgb(170,170,170)',
-        height:'30px',
-        width: '100px',
-        margin: '0px',
-        padding: '0px',
-        float:'right'
+      <div style={{
+        width:'768px',
       }}>
-        <option value="ko">한국어</option>
-        <option value="ko">English</option>
-        <option value="ko">中文(简体)</option>
-        <option value="ko">中文</option>
-      </select>
+        <div>
+        <div style={{
+          display:'flex',
+          justifyContent:'flex-end'
+        }}>
+          <select name="lan" id="lan" 
+            style={{
+                border: '1px solid #dadada',
+                height:'30px',
+                width: '96px',
+                margin: '0px',
+                padding: '0px',
+                float:'right',
+                color:'#4e4e4e'
+                }}>
+              <option value="ko">한국어</option>
+              <option value="ko">English</option>
+              <option value="ko">中文(简体)</option>
+              <option value="ko">中文</option>
+          </select>
+        </div>
       </div>
-      <div>
-      <h1 style={{
-      padding: '80px',
-      fontSize: '45px',
-      color:'rgb(3,199,90)',
-      fontWeight:'900',
-      textAlign:'center'
-    }}>NAVER</h1>
-      </div>
-      <div id='container' style={{
+
+        <div>
+        <h1 style={{
+        padding: '80px',
+        fontSize: '45px',
+        color:'rgb(3,199,90)',
+        fontWeight:'900',
+        textAlign:'center'
+        }}>NAVER</h1>
+        </div>
+
+        <form>
+        <section style={{
+          paddingTop:'60px',
+          display:'flex',
+          flexDirection:'column',
+          alignItems:'center'
+        }}>
+          <div style={{
+            width:'465px',
+            height:'48px',
+            border:'solid 1px #dadada',
+            background:'white'
+          }}>
+            <input placeholder ="Username" type="text" style={{
+              border:'none',
+              width:'430px',
+              marginTop:'10px',
+              fontSize:'14px',
+              marginLeft:'10px',
+              height:'30px'
+            }} />
+
+          </div>
+
+          <div style={{
+            marginTop: '13px',
+            width:'465px',
+            height:'48px',
+            border:'solid 1px #dadada',
+            background:'white'
+          }}>
+          <input placeholder ="Password" type="text" style={{
+              border:'none',
+              width:'430px',
+              marginTop:'10px',
+              fontSize:'14px',
+              marginLeft:'10px',
+              height:'30px'
+            }} />
+            
+          </div>
+
+
+          <div style={{
+            paddingTop: '13px'
+          }}>
+            <button style={{
+              width:'465px',
+              height:'48px',
+              fontSize:'18px',
+              background:'#04c75a',
+              color:'white',
+              border:'solid 1px #06b350'
+            }}>
+              Sign in
+            </button>
+          </div>
+
+          </section>
+        </form>
+
+          <div style={{
+            width:'768px',
+            height:'52px',
+            color:'#4e4e4e',
+            display:'flex',
+            alignItems:'center',
+            justifyContent:'center',
+            borderBottom:'solid 1px #dadada',
+            
+            
+          }}>
+            <span style={{
+              color:'#9ba1a3',
+              fontSize:'14px',
+              padding:'0px 10px',
+              lineHeight:'25px',
+              borderRight:'1px solid #dadada',
+            }}>비밀번호 찾기</span>
+            <span style={{
+              color:'#9ba1a3',
+              fontSize:'14px',
+              padding:'0px 10px',
+              lineHeight:'25px',
+              borderRight:'1px solid #dadada',
+
+            }}>아이디 찾기</span>
+            <span style={{
+              color:'#9ba1a3',
+              fontSize:'14px',
+              padding:'0px 10px',
+              lineHeight:'25px',
+            }}>회원가입</span>
+          </div>
+
         
-      }}>
-      <button>ID 로그인</button>
-      <button>일회용 번호</button>
-      <button>QR 코드</button>
+        
+        
+
+        <footer>
+          <div>
+            <span>
+              <img src="" alt="" />
+              Copyright © NAVER Corp. All Rights Reserved.
+            </span>
+          </div>
+        </footer>
+
       </div>
     </div>
 
