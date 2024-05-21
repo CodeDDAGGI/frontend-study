5b9f6689d5531c624b499512278d28f7424ab08e
CodeDDAGGI <154308455+CodeDDAGGI@users.noreply.github.com>
2024-05-21 22:35:04 +0900

    Add files via upload

--- mini project/todo/starbugs/main.js
+++ mini project/todo/starbugs/main.js
@@ -1,6 +1,8 @@
+
 const searchUI = document.querySelector('.search');
 const searchInput = document.querySelector('input');
 const icon = document.querySelector('.material-symbols-outlined');
+const navItem = document.querySelector('li');
 
 searchUI.addEventListener('click',function(){
     searchInput.focus();
@@ -18,6 +20,41 @@ searchInput.addEventListener('blur',function(){
     searchInput.setAttribute('placeholder', '');
     icon.style.opacity = '1';
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+
+  dropdownToggles.forEach(toggle => {
+    toggle.addEventListener('mouseenter', function () {
+      const targetId = this.getAttribute('data-target');
+      const dropdown = document.getElementById(targetId);
+      dropdown.style.display = 'grid';
+    });
+
+    toggle.addEventListener('mouseleave', function () {
+      const targetId = this.getAttribute('data-target');
+      const dropdown = document.getElementById(targetId);
+      dropdown.style.display = 'none';
+    });
+  });
+
+  const dropdowns = document.querySelectorAll('.dropdown-content');
+
+  dropdowns.forEach(dropdown => {
+    dropdown.addEventListener('mouseenter', function () {
+      this.style.display = 'grid';
+    });
+
+    dropdown.addEventListener('mouseleave', function () {
+      this.style.display = 'none';
+    });
+  });
+});
+
+
+
+
 
 // 
 //
